efa209c357f7e08c054b60587af684b2fd0fe6ba
Cory Spitzer <coryspitzer@gmail.com>
2016-02-16 15:55:38 -0500

    Return odd indexed elements

--- odd_positions.rb
+++ odd_positions.rb
@@ -1,3 +1,9 @@
 def odd_positions(list)
-  list
+  odds = []
+  list.each_with_index do |value, index|
+    unless index % 2 == 0
+      odds << value
+    end
+  end
+  odds
 end
--- odd_positions_spec.rb
+++ odd_positions_spec.rb
@@ -8,4 +8,16 @@ describe '#odd_positions' do
     expect(odd_positions([])).to eq []
   end
 
+  it 'handles a single-entry array' do
+    expect(odd_positions([7])).to eq []
+  end
+
+  it 'handles a double-entry array' do
+    expect(odd_positions([5, 6])).to eq [6]
+  end
+
+  it 'handles a multi-entry array' do
+    expect(odd_positions([5, 6, 7, 8])).to eq [6, 8]
+  end
+
 end
